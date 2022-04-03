81c17b8ba2b9ee1f2e53a761566afa15b408eeb8
33699 <brahim.idt1@gmail.com>
2022-04-03 23:49:11 +0200

    Try but not working but no error

--- app/src/main/java/com/example/todolist_ib/MainActivity.java
+++ app/src/main/java/com/example/todolist_ib/MainActivity.java
@@ -6,6 +6,7 @@ import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.view.View;
@@ -14,6 +15,22 @@ import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
 
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.*;
+
+import org.json.JSONObject;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 
@@ -21,6 +38,8 @@ public class MainActivity extends AppCompatActivity {
     private final static int MY_REQUEST_CODE = 1;
     ListView maListe;
     ArrayAdapter<String> myarray;
+
+
     String daily[] = { "Sacar al perro ; pediente", "comprar el pan ; pediente",
             "revisar el correo de la salle ; pediente", "preparar reuniones del día ; pediente",
             "hacer ejercicio ; pediente" };
@@ -34,7 +53,7 @@ public class MainActivity extends AppCompatActivity {
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        new fetchData().start();
         sharedPreferences = getSharedPreferences("Users", Context.MODE_PRIVATE);
         Button button = (Button) findViewById(R.id.addButton);
         String empty = "empty";
@@ -46,8 +65,8 @@ public class MainActivity extends AppCompatActivity {
                 startActivityForResult(intent, MY_REQUEST_CODE);
             }
         });
-    }
 
+    }
     public void ajt(String test){
 
         maListe = findViewById(R.id.list);
@@ -58,6 +77,7 @@ public class MainActivity extends AppCompatActivity {
             editor.putString(String.valueOf(i),String.valueOf(myArrayList.indexOf(i)));
         }
         editor.commit();
+
         maListe.setAdapter(myarray);
         maListe.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
@@ -71,13 +91,53 @@ public class MainActivity extends AppCompatActivity {
                 }else{
                     myArrayList.set(position,parties[0]+"; realisado");
                 }
+
                 myarray.notifyDataSetChanged();
 
             }
         });
     }
+class fetchData extends Thread{
 
-    @Override
+        String data = "";
+
+        @Override
+    public void run(){
+            try {
+                URL url = new URL("https://jsonplaceholder.typicode.com/todos");
+                HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+                InputStream inputSteam = httpURLConnection.getInputStream();
+                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputSteam));
+                String line;
+                while((line = bufferedReader.readLine()) != null){
+                    data = data+line;
+                }
+                if (!data.isEmpty()){
+                    JSONObject jsonObject = new JSONObject(data);
+                    JSONArray entity = jsonObject.getJSONArray("");
+                    for (int i =0; i< entity.length();i++){
+                        JSONObject object = entity.getJSONObject(i);
+                        String title = object.getString("title");
+                        String completed = object.getString("completed");
+                        if (completed == "false"){
+                            myArrayList.add(title+"; pediente");
+                        }else{
+                            myArrayList.add(title+"; realisado");
+                        }
+                    }
+                }
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+}
+
+
+        @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         if (resultCode == Activity.RESULT_OK) {
@@ -85,9 +145,7 @@ public class MainActivity extends AppCompatActivity {
                String test = data.getStringExtra("value");
                 myArrayList.add(test+" ; pediente");
                 myarray.notifyDataSetChanged();
-
             }
-
             SharedPreferences.Editor editor = sharedPreferences.edit();
             editor.clear();
             editor.commit();
